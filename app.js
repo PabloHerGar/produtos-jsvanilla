14a871c76d2a0502bf2caf373b15add7b84612d8
PabloHerGar <102415931+PabloHerGar@users.noreply.github.com>
2023-05-16 13:24:36 +0200

    Funcionalidad agregar producto y resetear formulario.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('hola mundo')
+const reset = function () {
+  document.getElementById('name').value = ''
+  document.getElementById('price').value = 0
+  document.getElementById('release').value = 2023
+}
+
+document.addEventListener('submit', function (event) {
+  const name = document.getElementById('name').value
+  const price = document.getElementById('price').value
+  const release = document.getElementById('release').value
+
+  console.log(name)
+  console.log(price)
+  console.log(release)
+
+  reset()
+
+  event.preventDefault() //Evita que la pagina de HTML se recarga, de este modo se mantienen los valores por consola
+})
+
+document.addEventListener('reset', reset())
+
+// console.log(name)
+// console.log(price)  No queremos devolver ningún valor
+// console.log(release)
